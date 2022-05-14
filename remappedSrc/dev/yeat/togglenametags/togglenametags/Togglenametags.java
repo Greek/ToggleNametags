1a4c29b8da9878eb203700160c22e7bd3a7fda66
Andreas <a@yeat.dev>
2022-05-14 14:10:49 -0400

    initial
    Signed-off-by: Andreas <a@yeat.dev>

---
+++ remappedSrc/dev/yeat/togglenametags/togglenametags/Togglenametags.java
@@ -0,0 +1,10 @@
+package dev.yeat.togglenametags.togglenametags;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Togglenametags implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
