41ea85b0a6828fc3f4f69ea8a2c917dc3c93b0c5
Andreas <a@yeat.dev>
2022-05-20 06:49:50 -0400

    wip reader

--- src/main/java/dev/yeat/togglenametags/Togglenametags.java
+++ src/main/java/dev/yeat/togglenametags/Togglenametags.java
@@ -1,10 +1,11 @@
 package dev.yeat.togglenametags;
 
+import dev.yeat.togglenametags.config.Reader;
 import net.fabricmc.api.ModInitializer;
 
 public class Togglenametags implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        Reader.register(true);
     }
 }
