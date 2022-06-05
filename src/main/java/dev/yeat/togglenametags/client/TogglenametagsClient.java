7fcb39d9043e1bac9fc2c06c114051fdc527519f
Andreas <a@yeat.dev>
2022-06-05 15:25:56 -0400

    Replace command with keybind

--- src/main/java/dev/yeat/togglenametags/client/TogglenametagsClient.java
+++ src/main/java/dev/yeat/togglenametags/client/TogglenametagsClient.java
@@ -8,6 +8,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.client.input.Input;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.server.command.CommandManager;
@@ -20,6 +21,7 @@ import org.lwjgl.glfw.GLFW;
 public class TogglenametagsClient implements ClientModInitializer {
 
     private static KeyBinding keyBinding;
+    private static KeyBinding keyBinding2;
     public static boolean shouldRender = true;
 
 
@@ -30,6 +32,13 @@ public class TogglenametagsClient implements ClientModInitializer {
                 InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
                 GLFW.GLFW_KEY_MINUS, // The keycode of the key
                 "Nametags" // The translation key of the keybinding's category.
+        ));
+
+        keyBinding2 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Toggle Other Nametags",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_EQUAL,
+                "Nametags"
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
@@ -44,21 +53,22 @@ public class TogglenametagsClient implements ClientModInitializer {
                             style.withColor(Formatting.DARK_GRAY)), Util.NIL_UUID);
                 }
             }
-        });
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            SimpleConfig config = SimpleConfig.of("ToggleNametagsConfig").provider(Reader::ltProvider).request();
+            while (keyBinding2.wasPressed()) {
+                SimpleConfig config = SimpleConfig.of("ToggleNametagsConfig").provider(Reader::ltProvider).request();
 
-            dispatcher.register(CommandManager.literal("toggleothernametags").executes(context -> {
                 if (Reader.renderOtherEntities) {
                     Reader.renderOtherEntities = false;
                     config.getOrDefault("render_all_except_players", false);
+                    client.player.sendSystemMessage(new LiteralText("Nametags from other entities are now hidden!").styled(style ->
+                            style.withColor(Formatting.DARK_GRAY)), Util.NIL_UUID);
                 } else {
                     Reader.renderOtherEntities = true;
                     config.getOrDefault("render_all_except_players", true);
+                    client.player.sendSystemMessage(new LiteralText("Nametags from other entities are now shown!").styled(style ->
+                            style.withColor(Formatting.DARK_GRAY)), Util.NIL_UUID);
                 }
-                return 1;
-            }));
+            }
         });
     }
 }
