fb90727eaac245260935bcce7664b15bc3cd118d
Andreas <a@yeat.dev>
2022-06-05 14:44:33 -0400

    Add value to render only other entity tags

--- src/main/java/dev/yeat/togglenametags/config/Reader.java
+++ src/main/java/dev/yeat/togglenametags/config/Reader.java
@@ -3,10 +3,19 @@ package dev.yeat.togglenametags.config;
 public class Reader {
 
     public static boolean renderOtherEntities = false;
+    public static boolean refreshingConfig;
 
     public static void register(boolean init) {
+        SimpleConfig config = SimpleConfig.of("ToggleNametagsConfig").provider(Reader::ltProvider).request();
 
+        if (init) {
+            renderOtherEntities = config.getOrDefault("render_all_except_players", renderOtherEntities);
+        }
     }
 
+    private static String ltProvider(String filename) {
+        return "# Toggle Nametags settings."
+                + "\nrender_all_except_players=" + renderOtherEntities;
+    }
 
 }
--- src/main/java/dev/yeat/togglenametags/mixin/RenderNametagMixin.java
+++ src/main/java/dev/yeat/togglenametags/mixin/RenderNametagMixin.java
@@ -1,20 +1,32 @@
 package dev.yeat.togglenametags.mixin;
 
 import dev.yeat.togglenametags.client.TogglenametagsClient;
+import dev.yeat.togglenametags.config.Reader;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
-public class RenderNametagMixin {
+public abstract class RenderNametagMixin<T extends Entity> {
     @Inject(at = @At("HEAD"), method = "render", cancellable = true)
-    private void doNotRender(CallbackInfo ci) {
+    private void doNotRender(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
         if (TogglenametagsClient.shouldRender) {
             return;
         } else {
-            ci.cancel();
+            if (Reader.renderOtherEntities) {
+                if (entity instanceof PlayerEntity) {
+                    ci.cancel();
+                }
+            } else {
+                ci.cancel();
+            }
         }
 
     }
