b62d794473cf3ba7910408eddc854aae5de5c7ea
Andreas <a@ndy.sh>
2024-01-17 13:58:19 -0500

    New code base + 1.20.4 update
    Signed-off-by: Andreas <a@ndy.sh>

---
+++ src/client/java/sh/ndy/mixin/client/MixinRenderSelfNameTag.java
@@ -0,0 +1,24 @@
+package sh.ndy.mixin.client;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import sh.ndy.config.Reader;
+
+@Mixin(LivingEntityRenderer.class)
+public class MixinRenderSelfNameTag<T extends LivingEntity> {
+    @Inject(
+            at = @At("HEAD"),
+            method = "hasLabel(Lnet/minecraft/entity/LivingEntity;)Z",
+            cancellable = true
+    )
+    private void viewOwnLabel(T livingEntity, CallbackInfoReturnable<Boolean> cir) {
+        if (Reader.renderOwnNametag && livingEntity == MinecraftClient.getInstance().cameraEntity) {
+            cir.setReturnValue(true);
+        };
+    }
+}
