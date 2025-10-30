f985d5c191fa9150aa02bf365306a95a86a4d35b
Andreas <git@fsck.rip>
2025-10-30 13:52:30 -0400

    chore: move render nametag mixin to listener file
    Signed-off-by: Andreas <git@fsck.rip>

--- src/client/java/sh/ndy/features/listeners/NametagsToggleListener.java
+++ src/client/java/sh/ndy/features/listeners/NametagsToggleListener.java
@@ -2,11 +2,17 @@ package sh.ndy.features.listeners;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.client.render.state.CameraRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import sh.ndy.config.Config;
 
-public class NametagsToggleListener implements IBaseBindingListener {
+public class NametagsToggleListener<T extends Entity, S extends EntityRenderState> implements IBaseBindingListener {
   public void handleBinding(MinecraftClient client, KeyBinding binding) {
 	if (client.player == null) return;
 
@@ -20,5 +26,13 @@ public class NametagsToggleListener implements IBaseBindingListener {
 	  client.player.sendMessage(Text.literal("Nametags are now hidden!").styled(style ->
 			  style.withColor(Formatting.DARK_GRAY)), false);
 	}
+  }
+
+  public void handleMixin(CallbackInfo ci) {
+	if (Config.getOptions().getRenderNametags()) {
+	  return;
+	}
+
+	ci.cancel();
   }
 }
