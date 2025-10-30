af4491f3bce28b4e438f006caaedf29b9af854d2
Andreas <git@fsck.rip>
2025-10-30 13:52:47 -0400

    chore: remove unused imports
    Signed-off-by: Andreas <git@fsck.rip>

--- src/client/java/sh/ndy/mixin/client/MixinRenderSelfNameTag.java
+++ src/client/java/sh/ndy/mixin/client/MixinRenderSelfNameTag.java
@@ -1,13 +1,11 @@
 package sh.ndy.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import sh.ndy.config.Config;
 import sh.ndy.features.listeners.SelfNametagToggleListener;
 
 @Mixin(LivingEntityRenderer.class)
