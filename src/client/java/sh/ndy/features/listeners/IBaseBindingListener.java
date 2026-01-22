5c3fcb51e46279eaf5a4756326ff8b19a5a15ad7
Andreas <a@ndy.sh>
2026-01-21 21:23:59 -0500

    chore: make `handleBinding` abstract

--- src/client/java/sh/ndy/features/listeners/IBaseBindingListener.java
+++ src/client/java/sh/ndy/features/listeners/IBaseBindingListener.java
@@ -4,6 +4,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 
 interface IBaseBindingListener<T> {
-  void handleBinding(MinecraftClient client, KeyBinding binding);
-  static void handleMixin() {}
+    static void handleBinding(MinecraftClient client, KeyBinding binding) {
+
+    }
+
+    static void handleMixin() {}
 }
