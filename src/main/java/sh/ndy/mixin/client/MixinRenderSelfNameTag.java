8a831ff87cf9a2481bcbf3372eb4527abe73d36c
Andreas <a@ndy.sh>
2026-04-05 01:16:28 -0400

    fix: add entity import

--- src/main/java/sh/ndy/mixin/client/MixinRenderSelfNameTag.java
+++ src/main/java/sh/ndy/mixin/client/MixinRenderSelfNameTag.java
@@ -1,6 +1,7 @@
 package sh.ndy.mixin.client;
 
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
